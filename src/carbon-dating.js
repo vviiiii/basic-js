93ea0fcb7091e7013acb2bedd9f95850c9dd0719
Siarhei Liavontsyeu <leontevsi@gmail.com>
2020-02-18 22:27:01 +0300

    feat: implement carbonDating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    //throw 'Not implemented';
+    if (typeof sampleActivity != 'string' ||
+        Number.isNaN(parseFloat(sampleActivity)) ||
+        parseFloat(sampleActivity) > 15 ||
+        parseFloat(sampleActivity) <= 0) return false;
+
+    let rateConstant = 0.693 / HALF_LIFE_PERIOD,
+        year = (Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / rateConstant;
+
+    return Math.ceil(year);
 };
