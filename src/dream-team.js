3a8bed350ead86eacf975dcc564a2a6d86570e68
Siarhei Liavontsyeu <leontevsi@gmail.com>
2020-02-19 10:58:32 +0300

    feat: implement dreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    //throw 'Not implemented';
+    let secretName = '';
+    if (!Array.isArray(members)) return false;
+
+    secretName = members.filter(el => typeof el === 'string').map(element => {
+        return element.trim()[0].toUpperCase();
+    });
+
+    return secretName.sort().join('');
 };
