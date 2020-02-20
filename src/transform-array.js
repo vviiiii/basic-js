3370ee6dd05eb486396f083bbffa6556a284ed0c
Siarhei Liavontsyeu <leontevsi@gmail.com>
2020-02-20 12:28:00 +0300

    feat: implement transformArray

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    //throw 'Not implemented';
+    let resArr = [];
+
+    if (!Array.isArray(arr)) throw new Error('Not array');
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            if (i + 1 < arr.length) {
+                i++;
+            }
+        } else if (arr[i] === '--discard-prev') {
+            resArr.pop();
+        } else if (arr[i] === '--double-prev') {
+            if (arr[i - 1] !== undefined) {
+                resArr.push(arr[i - 1]);
+            }
+        } else if (arr[i] === '--double-next') {
+            if (arr[i + 1] !== undefined) {
+                resArr.push(arr[i + 1]);
+            }
+        } else {
+            resArr.push(arr[i]);
+        }
+    }
+    return resArr;
 };
