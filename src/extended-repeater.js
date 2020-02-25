67a706b39aae1eaa772dee22d58e897c56adc60d
Siarhei Liavontsyeu <leontevsi@gmail.com>
2020-02-25 10:56:27 +0300

    feat: implement extendedRepeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,31 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+function repeatString(str, repeatTimes, separator, addition) {
+    if (+repeatTimes <= 1) return str;
+    else return str + separator + repeatString(str, +repeatTimes - 1, separator);
+}
+
+module.exports = function repeater(str, options) {
+    //throw 'Not implemented';
+    let { repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|' } = options;
+    let resString = '' + str;
+    let result = '';
+
+    if ('' + addition != 'undefined') {
+        for (let j = 0; j < additionRepeatTimes; j++) {
+            if (j === 0) {
+                resString += addition;
+            } else {
+                resString += additionSeparator + addition;
+            }
+        }
+    }
+
+    for (let i = 0; i < repeatTimes; i++) {
+        if (i === 0) {
+            result += resString;
+        } else {
+            result += separator + resString;
+        }
+
+    }
+    return result;
+}
